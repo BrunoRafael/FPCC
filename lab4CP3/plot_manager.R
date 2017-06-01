f762492ff4ec3146fd3a67f90c5cf3043b5a075d
BrunoRafael <bruno.rafael.vasconcelos@ccc.ufcg.edu.br>
2017-06-01 02:47:11 -0300

    Finalizando o relatório

--- lab4CP3/plot_manager.R
+++ lab4CP3/plot_manager.R
@@ -8,12 +8,12 @@ plot_median_ic = function(data){
     ggplot(aes(x=median.users.)) +
     geom_histogram(binwidth = 10, colour = "#6ba82f", fill = "darkgreen") + 
     geom_vline(aes(xintercept = df.median$X2.5.), colour = "blue") + 
-    geom_text(aes(x = df.median$X2.5., y = 500, label = "Início do intervalo"))+
+    geom_text(aes(x = df.median$X2.5., y = 500, label = "InÃ­cio do intervalo"))+
     geom_vline(aes(xintercept = df.median$X97.5.), colour = "blue") + 
     geom_text(aes(x = df.median$X97.5., y = 1500,  label = "Fim do intervalo"))+
     geom_vline(aes(xintercept = data$observed), colour = "darkorange")+
-    geom_text(aes(x = data$observed, y = 1000, label = "Mediana da amostra"))+
-    labs(x = "Medianas das amostras", y = "Frequência de medianas")
+    geom_text(aes(x = data$observed, y = 1000, label = "Desvios padrÃ£o da amostra"))+
+    labs(x = "Medianas das amostras", y = "FrequÃªncia de medianas")
 }
 
 
@@ -26,10 +26,10 @@ plot_sd_ic = function(data){
     ggplot(aes(x=sd.users.)) +
     geom_histogram(binwidth = 10, colour = "#6ba82f", fill = "darkgreen") + 
     geom_vline(aes(xintercept = df.sd$X2.5.), colour = "blue") + 
-    geom_text(aes(x = df.sd$X2.5., y = 500, label = "Início do intervalo"))+
+    geom_text(aes(x = df.sd$X2.5., y = 500, label = "InÃ­cio do intervalo"))+
     geom_vline(aes(xintercept = df.sd$X97.5.), colour = "blue") + 
     geom_text(aes(x = df.sd$X97.5., y = 1500,  label = "Fim do intervalo"))+
     geom_vline(aes(xintercept = data$observed), colour = "darkorange")+
     geom_text(aes(x = data$observed, y = 1000, label = "Mediana da amostra"))+
-    labs(x = "Medianas das amostras", y = "Frequência de medianas")
+    labs(x = "Medianas das amostras", y = "FrequÃªncia de medianas")
 }
